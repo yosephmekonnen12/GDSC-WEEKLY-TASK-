73abeb89e909b16eb05f0a186f9ade5c665b2696
yosephmekonnen12 <151457329+yosephmekonnen12@users.noreply.github.com>
2023-12-05 21:00:31 +0300

    Create two.py

---
+++ week3/two.py
@@ -0,0 +1,17 @@
+character=input("please ensert character ")
+
+for line in range(1,6):
+  
+  if  line ==1:
+    print(character)
+  elif line==2:
+    print(character*2)
+  elif line==3:
+    print(character*3)
+  elif line==4:
+    print(character*4)
+  elif line==5:
+    print(character*5)
+  
+    
+  
