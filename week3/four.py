8ab0908384b20f1c1047ed04f0f19046c25fb427
yosephmekonnen12 <151457329+yosephmekonnen12@users.noreply.github.com>
2023-12-05 20:04:17 +0300

    Create four.py

---
+++ week3/four.py
@@ -0,0 +1,13 @@
+sum=0
+for nums in range(1,50):
+  
+  if nums%2==0:
+    sum+=nums
+  elif nums%3==0:
+    sum+=nums
+    print("three")
+  elif nums%5==0:
+    sum+=
+    print("five")
+  else:
+    print(nums)
