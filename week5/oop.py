bd718a495df3b71263180e2915f22149e22cdd02
yosephmekonnen12 <151457329+yosephmekonnen12@users.noreply.github.com>
2023-12-26 15:05:39 +0300

    Update oop.py

--- week5/oop.py
+++ week5/oop.py
@@ -32,27 +32,6 @@ electricar.einfo()
 
 
 
-#bank class
-class BankAccount:
-    def __init__(self):
-        self.__balance = 0
-
-    def deposit(self, amount):
-        self.__balance += amount
-
-    def withdraw(self, amount):
-        if amount > self.__balance:
-            print("Insufficient balance")
-        else:
-            self.__balance -= amount
-
-    def display_balance(self):
-        return self.__balance
-account = BankAccount()
-account.deposit(1000)
-account.withdraw(500)
-print("Current balance:", account.display_balance())
-
 
 
 
